52ad9bd50e3f8bd18bdcabb7509603bf4bb410c6
Julia <juliakuntsevich@gmail.com>
2020-03-23 18:47:33 +0100

    Implement Abbreviation

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,22 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    if (b.length == 0) {
+        return @"YES";
+    }
+    
+    NSString *uppercaseA = [a uppercaseString];
+    int j = 0;
+    for (int i = 0; i < uppercaseA.length; i++) {
+        if ([uppercaseA characterAtIndex:i] == [b characterAtIndex:j]) {
+            j += 1;
+        }
+        
+        if (j >= b.length) {
+            return @"YES";
+        }
+    }
+    
+    return @"NO";
 }
 @end
